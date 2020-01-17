920c56c2c8c78fee76af15ff4fb1cb9a30a711be
kelvin.luvila <kelvin.luvila@szi.gov.zm>
2020-01-17 12:25:41 +0200

    Feature: Added StatisticsCalculator

---
+++ ClassLibrary1/Subtract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Operations;
+
+namespace Subtract
+{
+    public interface ISub
+    {
+        public dynamic Subtraction(dynamic a, dynamic b);
+    }
+    public class Sub : ISub
+    {
+        public dynamic result;
+
+        public dynamic Subtraction(dynamic a, dynamic b)
+        {
+            result = Operations.Subtraction.Sub(a, b);
+            return result;
+        }
+    }
+}
